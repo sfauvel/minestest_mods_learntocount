6f6370e3dca7c6aa8be6e15e3e8c35afcf2704c7
sfauvel <sfauvel@gmail.com>
2020-11-17 13:27:57 +0100

    Refactor mapgen

--- ltc_mapgen.lua
+++ ltc_mapgen.lua
@@ -1,37 +1,41 @@
 
-minetest.register_on_generated(function(minp, maxp, seed)
-	local debug = "minp="..(minetest.pos_to_string(minp))..", maxp="..(minetest.pos_to_string(maxp))..", seed="..seed
-    minetest.log(debug)
-    --print(debug)
-	--minetest.chat_send_all(debug)
+local function log_position(message, position)
+    minetest.log(message..position.x.."/"..position.y.."/"..position.z)
+end
 
-	local manip = minetest.get_voxel_manip()
-	local e1, e2 = manip:read_from_map(minp, maxp)
-	local area = VoxelArea:new{MinEdge=e1, MaxEdge=e2}
-
-	local data = manip:get_data()
-
-    local function log_position(message, position)
-        minetest.log(message..position.x.."/"..position.y.."/"..position.z)
-    end
-
-	local function is_in_air(area, position, length)
-		local c_air = minetest.get_content_id("air")	
-		for index=0,length do
-			local vi = area:index(position.x+index, position.y, position.z)
-			if data[vi] == c_air then
-				return true
-			end 
-		end
-        --log_position("Not in air:", position)
-		return false
-	end
-
+local function is_in_air(data, area, position, length)
     local c_air = minetest.get_content_id("air")	
-    local midx = minp.x+math.floor((maxp.x-minp.x)/2)
-    local midy = minp.y+math.floor((maxp.y-minp.y)/2)
-    local midz = minp.z+math.floor((maxp.z-minp.z)/2)
-    minetest.log("Mid x:"..midx.." y:"..midy.." z:"..midz )
+    for index=0,length do
+        local vi = area:index(position.x+index, position.y, position.z)
+        if data[vi] == c_air then
+            return true
+        end 
+    end
+    --log_position("Not in air:", position)
+    return false
+end
+
+
+
+local function add_some_simple_symbols(minp, maxp, data, area) 
+    local c_air = minetest.get_content_id("air")	
+
+    local function add_symbol_on_surface(positions_to_add, area, x, z, startY, endY) 
+        for y=startY,endY,1 do
+            local vi = area:index(x, y, z)
+            local under = area:index(x, y-1, z)
+
+            if data[vi] == c_air  and data[under] ~= c_air then
+                local name = minetest.get_node(vector.new(x,y-1,z)).name
+                if minetest.get_item_group(name, "flower") == 0 
+                    or name == "default:snow"
+                    or string.sub(name,1,string.len("flowers:"))=="flowers" then
+                        vi = area:index(x, y-1, z)
+                end
+                table.insert(positions_to_add, vi)
+            end 
+        end
+    end
 
 
     local positions_to_add={}
@@ -44,27 +48,8 @@ minetest.register_on_generated(function(minp, maxp, seed)
             if current >= next then
                 current = 0
                 next = math.random(1,FREQUENCY)
-                for y=minp.y,maxp.y,1 do
-                    local vi = area:index(x, y, z)
-                    local under = area:index(x, y-1, z)
 
-                   -- local name = minetest.get_node(vector.new(x,y-1,z)).name
-                   -- if minetest.get_item_group(name, "sand") ~= 0
-                   --     or minetest.get_item_group(name, "wood") ~= 0
-                   --     or minetest.get_item_group(name, "stone") ~= 0 then
-                   --         table.insert(positions_to_add, vi)
-                   -- end
-
-                    if data[vi] == c_air  and data[under] ~= c_air then
-                        local name = minetest.get_node(vector.new(x,y-1,z)).name
-                        if minetest.get_item_group(name, "flower") == 0 
-                            or name == "default:snow"
-                            or string.sub(name,1,string.len("flowers:"))=="flowers" then
-                                vi = area:index(x, y-1, z)
-                        end
-                        table.insert(positions_to_add, vi)
-                    end 
-                end
+                add_symbol_on_surface(positions_to_add, area, x, z, minp.y,maxp.y)
             end
         end
     end
@@ -73,28 +58,11 @@ minetest.register_on_generated(function(minp, maxp, seed)
         local symbol = minetest.get_content_id("learntocount:symbol_" .. math.random(0,9))	 
         data[p] = symbol
     end
+end
 
+local function add_equations(minp, maxp, data, area) 
     local direction = vector.new(1,0,0)
     
-   -- for x=minp.x,maxp.x,10 do
-	--	for z=minp.z,maxp.z,10 do
-	--		for y=minp.y,maxp.y,3 do
-	--			local formula = FormulaGenerator.generate()		
-   --             if not (is_in_air(area, vector.new(x, y, z), table.getn(formula))) then
-   --               --  log_position("Build in:", vector.new(x, y, z))
-	--				for index, value in pairs(formula) do
-	--					local vi = area:index(x+index, y, z)
-	--					local symbol = minetest.get_content_id("learntocount:symbol_fix_" .. value)
-	--					data[vi] = symbol
-	--				end
-	--			
-	--			end
-	--			
-	--		end
-	--	end
-   -- end
-   -- 
-
     local x = minp.x
     while x < maxp.x do
         local z = minp.z
@@ -102,7 +70,7 @@ minetest.register_on_generated(function(minp, maxp, seed)
             local y = minp.y
             while y < maxp.y do
                 local formula = FormulaGenerator.generate()		
-                if not (is_in_air(area, vector.new(x, y, z), table.getn(formula))) then
+                if not (is_in_air(data, area, vector.new(x, y, z), table.getn(formula))) then
                     --log_position("Build in:", vector.new(x, y, z))
 					for index, value in pairs(formula) do
 						local vi = area:index(x+index, y, z)
@@ -117,7 +85,22 @@ minetest.register_on_generated(function(minp, maxp, seed)
         end
         x = x + math.random(1, 20)
     end
+end
 
+
+minetest.register_on_generated(function(minp, maxp, seed)
+	local debug = "minp="..(minetest.pos_to_string(minp))..", maxp="..(minetest.pos_to_string(maxp))..", seed="..seed
+    --minetest.log(debug)
+    --print(debug)
+	--minetest.chat_send_all(debug)
+
+	local manip = minetest.get_voxel_manip()
+	local e1, e2 = manip:read_from_map(minp, maxp)
+	local area = VoxelArea:new{MinEdge=e1, MaxEdge=e2}
+	local data = manip:get_data()
+
+    add_some_simple_symbols(minp, maxp, data, area)
+    add_equations(minp, maxp, data, area)
 
 	manip:set_data(data)
 	manip:write_to_map()
